80723f5965af8f9c826e6fac250b3d0d144cf2dc
Kartik Varshney <codehacker05@gmail.com>
2026-02-12 01:26:08 +0530

    updatec BASE_URL and Nginx Config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "/api";
 
 export const USER_ICON =
   "https://img.freepik.com/free-psd/contact-icon-illustration-isolated_23-2151903337.jpg?semt=ais_hybrid&w=740&q=80";
