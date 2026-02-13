a23331a5b0c8f791b16cd9c7afb1cd102fef67d8
Kartik Varshney <codehacker05@gmail.com>
2026-02-14 00:57:15 +0530

    Made Base URL Dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
 export const USER_ICON =
   "https://img.freepik.com/free-psd/contact-icon-illustration-isolated_23-2151903337.jpg?semt=ais_hybrid&w=740&q=80";
